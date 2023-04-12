0bb3dce2d8f3a9b4b3ea61009c895f83c2380229
ehlegault <etiennehlegault@gmail.com>
2023-04-12 12:43:37 -0400

    retry only if 404 redirect otherwise

--- script.js
+++ script.js
@@ -4,12 +4,14 @@
     if (tryCount <= 0) return
 
     const url = "https://blog-7hp5u76fha-uc.a.run.app/"
-    fetch(url, {cache: "no-cache", mode: "no-cors"}).then(res => {
-        console.log(res, "res")
+    fetch(url, {cache: "no-cache"}).then(res => {
+        console.log(res.status, "res")
         if (res.status != 404)
         {
             window.location = url
         }
+    }).catch(err => {
+        window.location = url
     })
 
     tryCount--;
